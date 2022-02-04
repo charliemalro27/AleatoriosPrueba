577e97c5984ff7e8187e4ee5aceab363a48d4187
charliemalro27 <carlos.maldonado@itgranada.com>
2022-02-04 18:12:10 +0100

    Funcion "Rellenar lista con 9 0s y un 1"

--- Codigo.cpp
+++ Codigo.cpp
@@ -13,22 +13,28 @@ for(int count=0;count<tamano;count++){
      if(numero>5){
          vector[count]=numero;
          count+=1;
-     }
- }while(count<=tamano);
- }
- }
-
+   }
+}while(count<=tamano);
+}
+}
+//Funcion rellenar lista con 9 0s y un 1
+void rellenarcon0(int vector[],int tamano){
+  for(int i=0;i<tamano;i++){
+    vector[i]=0;
+    vector[tamano-1]=1;  
+}
+}
 //Funcion Imprimir lista
 void imprimirlista(int lista[],int tamano){
   for(int i=0;i<tamano;i++){
     cout<<lista[i]<<endl;
-  }
 }
-
+}
 int main(){
 
  int numero=0;
  int lista[10];
+ int lista0[10];
 
 //Nombre
 cout<<"Nombre: Soy Charlieeee"<<endl;
@@ -37,6 +43,11 @@ cout<<"Nombre: Soy Charlieeee"<<endl;
 rellenarmayor5(lista,10);
 cout<<"Esta es la lista de numeros"<<endl;
 imprimirlista(lista,10);
+
+//Rellenar lisat con funcionar rellenarcon0
+rellenarcon0(lista0,10);
+cout<<"Esta es la lista de 0 y 1"<<endl;
+imprimirlista(lista0,10);
 
   return 0;
 }
